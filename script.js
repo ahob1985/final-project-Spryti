7b620448d916c5f530df7d01e2353bf96a207ad5
Spryti <67284103+Spryti@users.noreply.github.com>
2021-07-21 21:09:54 +0000

    first commit

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
-// Author:
+// Author: Jordan Muturi
 
 // Global UI Variables
-//let canvasDiv;
+let canvasDiv;
+let canvas;
+let button;
+let buttonDiv;
+
+
 
 // Global ML Variables
+let soundClassifier
+let isModelReady
+//https://teachablemachine.withgoogle.com/models/ygn8eLEP0/model.json
 
 function setup() {
-
+  canvasDiv = createDiv();
+  canvas = createCanvas(640, 480);
+  canvas.parent(canvasDiv);
+  background(0, 100, 200);
+  buttonDiv = createDiv();
+  button = createButton("Listen");
+  button.parent(buttonDiv);
+  soundClassifier = ml5.soundClassifier
 }
 
 function draw() {
 
 }
 
-function gotResults(error, results) {
+function isModelReady() {
+  
+}
 
+function checkAnswer() {
+
+}
+
+function gotResults(error, results) {
+  if(error) {
+    console.error(error);
+  } else {
+    let label = results[0].label;
+    let confidence =  round(results[0].confidence, 2);
+  }
 }
